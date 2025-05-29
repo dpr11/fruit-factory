ab223c379725f0597580516fb9454da7743f3895
David Raftis <dpr11@mac.com>
2025-05-29 19:31:08 +0100

    UI changes

--- FruitPicker/FruitPicker/ContentView.swift
+++ FruitPicker/FruitPicker/ContentView.swift
@@ -18,7 +18,7 @@ struct ContentView: View {
                 .padding()
             Text("Fruit Picker").font(.title2)
                 .padding()
-            Text("Catalogue Condition Revamp").font(.subheadline).italic()
+            Text("Catalogue Conditions Uncovered").font(.subheadline).italic()
         }
         .padding()
     }
